52b63fbb22a69640567ae9c6881edcc681d383c9
reikasar <183247743+reikasar@users.noreply.github.com>
2024-11-10 01:15:07 +0200

    add bug fix where id selector was missing #

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const input = document.querySelector("#todoInput");
 const addButton = document.querySelector("#addButton");
-const ul = document.querySelector("todoList");
+const ul = document.querySelector("#todoList");
 
 function addItem() {
   const inputValue = input.value;
+  if (inputValue == "") return;
   const listItem = document.createElement("li");
+  //console.log(listItem);
   const span = document.createElement("span");
   span.textContent = inputValue;
   const deleteBotton = document.createElement("button");
   deleteBotton.textContent = "remove";
-  deleteBotton.style.marginLeft = "10px";
+  deleteBotton.style.margin = "5px";
   listItem.appendChild(span);
+  //console.log(span);
   listItem.appendChild(deleteBotton);
   ul.appendChild(listItem);
+  //console.log(ul);
   deleteBotton.addEventListener("click", () => {
     ul.removeChild(listItem);
     input.focus()
@@ -22,4 +26,5 @@ function addItem() {
 }
 
 addButton.addEventListener("click", addItem);
+
 
